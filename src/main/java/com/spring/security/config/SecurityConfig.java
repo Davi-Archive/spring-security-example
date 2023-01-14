4b33364ca94fc0b409816ae5a0838246c8ddf8f2
d1av <davi4alves@gmail.com>
2023-01-14 00:12:26 -0300

    feat: add once per request filter

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
+@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -16,8 +19,6 @@ public class SecurityConfig {
                 .and()
                 .httpBasic();
 
-
         return http.build();
     }
-
 }
